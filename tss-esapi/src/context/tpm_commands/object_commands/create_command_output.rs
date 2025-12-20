274f174bfd1178b6a35da33c4b6fb136d857fd77
Jesper Brynolf <jesper.brynolf@gmail.com>
2025-12-20 18:27:16 +0100

    Improves the handling Create command output data.
    Signed-off-by: Jesper Brynolf <jesper.brynolf@gmail.com>

--- tss-esapi/src/context/tpm_commands/object_commands/create_command_output.rs
+++ tss-esapi/src/context/tpm_commands/object_commands/create_command_output.rs
@@ -2,91 +2,150 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    ffi::take_from_esys,
+    ffi::{data_zeroize::FfiDataZeroize, take_from_esys},
     structures::{CreateKeyResult, CreationData, CreationTicket, Digest, Private, Public},
     tss2_esys::{TPM2B_CREATION_DATA, TPM2B_DIGEST, TPM2B_PRIVATE, TPM2B_PUBLIC, TPMT_TK_CREATION},
     Error, Result,
 };
-use std::convert::TryFrom;
-use std::ptr::null_mut;
+use std::{convert::TryFrom, ops::Drop, ptr::null_mut};
 
 /// Struct that handles the output of the
 /// Create Esys_Create command and zeroizes
 /// the FFI data.
+///
+/// # Details
+/// Makes sure that no data is left on the heap when
+/// the object gets dropped.
 pub(crate) struct CreateCommandOutputHandler {
-    ffi_out_public_ptr: *mut TPM2B_PUBLIC,
-    ffi_out_private_ptr: *mut TPM2B_PRIVATE,
-    ffi_creation_data_ptr: *mut TPM2B_CREATION_DATA,
-    ffi_creation_hash_ptr: *mut TPM2B_DIGEST,
-    ffi_creation_ticket_ptr: *mut TPMT_TK_CREATION,
+    // They are stored as Option to be able to indicate
+    // to the drop function if there is any data on the
+    // heap that it needs to handle.
+    ffi_out_public_ptr: Option<*mut TPM2B_PUBLIC>,
+    ffi_out_private_ptr: Option<*mut TPM2B_PRIVATE>,
+    ffi_creation_data_ptr: Option<*mut TPM2B_CREATION_DATA>,
+    ffi_creation_hash_ptr: Option<*mut TPM2B_DIGEST>,
+    ffi_creation_ticket_ptr: Option<*mut TPMT_TK_CREATION>,
 }
 
 /// Creates a new CreateCommandOutputHandler
 impl CreateCommandOutputHandler {
     pub(crate) fn new() -> Self {
         Self {
-            ffi_out_private_ptr: null_mut(),
-            ffi_out_public_ptr: null_mut(),
-            ffi_creation_data_ptr: null_mut(),
-            ffi_creation_hash_ptr: null_mut(),
-            ffi_creation_ticket_ptr: null_mut(),
+            ffi_out_private_ptr: Some(null_mut()),
+            ffi_out_public_ptr: Some(null_mut()),
+            ffi_creation_data_ptr: Some(null_mut()),
+            ffi_creation_hash_ptr: Some(null_mut()),
+            ffi_creation_ticket_ptr: Some(null_mut()),
         }
     }
 
     /// A reference to the where 'outPrivate' output parameter pointer shall be stored.
     pub fn ffi_out_private_ptr(&mut self) -> &mut *mut TPM2B_PRIVATE {
-        &mut self.ffi_out_private_ptr
+        // The unwrap is will always be Some until its dropped.
+        self.ffi_out_private_ptr.as_mut().unwrap()
     }
 
     /// A reference to the where 'outPublic' output parameter pointer shall be stored.
     pub fn ffi_out_public_ptr(&mut self) -> &mut *mut TPM2B_PUBLIC {
-        &mut self.ffi_out_public_ptr
+        // The unwrap is will always be Some until its dropped.
+        self.ffi_out_public_ptr.as_mut().unwrap()
     }
 
     /// A reference to the where 'creationData' output parameter pointer shall be stored.
     pub fn ffi_creation_data_ptr(&mut self) -> &mut *mut TPM2B_CREATION_DATA {
-        &mut self.ffi_creation_data_ptr
+        // The unwrap is will always be Some until its dropped.
+        self.ffi_creation_data_ptr.as_mut().unwrap()
     }
 
     /// A reference to the where 'creationHash' output parameter pointer shall be stored.
     pub fn ffi_creation_hash_ptr(&mut self) -> &mut *mut TPM2B_DIGEST {
-        &mut self.ffi_creation_hash_ptr
+        // The unwrap is will always be Some until its dropped.
+        self.ffi_creation_hash_ptr.as_mut().unwrap()
     }
 
     /// A reference to the where 'creationTicket' output parameter pointer shall be stored.
     pub fn ffi_creation_ticket_ptr(&mut self) -> &mut *mut TPMT_TK_CREATION {
-        &mut self.ffi_creation_ticket_ptr
+        // The unwrap is will always be Some until its dropped.
+        self.ffi_creation_ticket_ptr.as_mut().unwrap()
     }
 }
 
 impl TryFrom<CreateCommandOutputHandler> for CreateKeyResult {
     type Error = Error;
 
-    fn try_from(ffi_data_handler: CreateCommandOutputHandler) -> Result<CreateKeyResult> {
-        // Take and free with Esys_Free; then null out the handler's fields so Drop (if any)
-        // won't free them a second time.
+    fn try_from(mut ffi_data_handler: CreateCommandOutputHandler) -> Result<CreateKeyResult> {
+        // The way things are done is very intentional. All data needs to be converted to owned
+        // so that the data allocated on the heap gets zeroized and freed. It is then as soon
+        // as possible converted to native types so that the sensitive data is stored in structures
+        // that can zeroize it. After these steps are taken it is relatively safe to check for
+        // errors and return the result.
+        let out_private_result = ffi_data_handler
+            .ffi_out_private_ptr
+            .take()
+            .ok_or(Error::local_error(crate::WrapperErrorKind::InternalError)) // Should never happen
+            .and_then(|ptr| unsafe { take_from_esys(ptr) })
+            .and_then(Private::try_from);
 
-        let out_private_owned_result =
-            unsafe { take_from_esys(ffi_data_handler.ffi_out_private_ptr) };
+        let out_public_result = ffi_data_handler
+            .ffi_out_public_ptr
+            .take()
+            .ok_or(Error::local_error(crate::WrapperErrorKind::InternalError)) // Should never happen
+            .and_then(|ptr| unsafe { take_from_esys(ptr) })
+            .and_then(Public::try_from);
 
-        let out_public_owned_result =
-            unsafe { take_from_esys(ffi_data_handler.ffi_out_public_ptr) };
+        let creation_data_result = ffi_data_handler
+            .ffi_creation_data_ptr
+            .take()
+            .ok_or(Error::local_error(crate::WrapperErrorKind::InternalError)) // Should never happen
+            .and_then(|ptr| unsafe { take_from_esys(ptr) })
+            .and_then(CreationData::try_from);
 
-        let creation_data_owned_result =
-            unsafe { take_from_esys(ffi_data_handler.ffi_creation_data_ptr) };
+        let creation_hash_result = ffi_data_handler
+            .ffi_creation_hash_ptr
+            .take()
+            .ok_or(Error::local_error(crate::WrapperErrorKind::InternalError)) // Should never happen
+            .and_then(|ptr| unsafe { take_from_esys(ptr) })
+            .and_then(Digest::try_from);
 
-        let creation_hash_owned_result =
-            unsafe { take_from_esys(ffi_data_handler.ffi_creation_hash_ptr) };
-
-        let creation_ticket_owned_result =
-            unsafe { take_from_esys(ffi_data_handler.ffi_creation_ticket_ptr) };
+        let creation_ticket_result = ffi_data_handler
+            .ffi_creation_ticket_ptr
+            .take()
+            .ok_or(Error::local_error(crate::WrapperErrorKind::InternalError)) // Should never happen
+            .and_then(|ptr| unsafe { take_from_esys(ptr) })
+            .and_then(CreationTicket::try_from);
 
         Ok(CreateKeyResult {
-            out_private: out_private_owned_result.and_then(Private::try_from)?,
-            out_public: out_public_owned_result.and_then(Public::try_from)?,
-            creation_data: creation_data_owned_result.and_then(CreationData::try_from)?,
-            creation_hash: creation_hash_owned_result.and_then(Digest::try_from)?,
-            creation_ticket: creation_ticket_owned_result.and_then(CreationTicket::try_from)?,
+            out_private: out_private_result?,
+            out_public: out_public_result?,
+            creation_data: creation_data_result?,
+            creation_hash: creation_hash_result?,
+            creation_ticket: creation_ticket_result?,
         })
+    }
+}
+
+impl Drop for CreateCommandOutputHandler {
+    fn drop(&mut self) {
+        // Make sure we do not have anything allocated on the heap if the handler gets dropped
+        // and that the owned data gets zeroized.
+        if let Some(ptr) = self.ffi_out_private_ptr.take() {
+            let _ = unsafe { take_from_esys(ptr) }.map(|mut v| v.ffi_data_zeroize());
+        }
+
+        if let Some(ptr) = self.ffi_out_public_ptr.take() {
+            let _ = unsafe { take_from_esys(ptr) }.map(|mut v| v.ffi_data_zeroize());
+        }
+
+        if let Some(ptr) = self.ffi_creation_data_ptr.take() {
+            let _ = unsafe { take_from_esys(ptr) }.map(|mut v| v.ffi_data_zeroize());
+        }
+
+        if let Some(ptr) = self.ffi_creation_hash_ptr.take() {
+            let _ = unsafe { take_from_esys(ptr) }.map(|mut v| v.ffi_data_zeroize());
+        }
+
+        if let Some(ptr) = self.ffi_creation_ticket_ptr.take() {
+            let _ = unsafe { take_from_esys(ptr) }.map(|mut v| v.ffi_data_zeroize());
+        }
     }
 }
